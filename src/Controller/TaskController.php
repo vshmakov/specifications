33b4effe56e4738597b37056e4bbde8a4b647eef
Vadim Shmakov <shmakovvadim@mail.ru>
2021-01-24 12:32:19 +0500

    [task] Added controller

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/task')]
+final class TaskController extends AbstractController
+{
+    #[Route('/', name: 'task_index', methods: ['GET'])]
+    public function index(TaskRepository $taskRepository): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'tasks' => $taskRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'task_show', methods: ['GET'])]
+    public function show(Task $task): Response
+    {
+        return $this->render('task/show.html.twig', [
+            'task' => $task,
+        ]);
+    }
+}
