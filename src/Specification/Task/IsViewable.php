43754496fd2cfdc0f216c4d268ef545b15adacad
Vadim Shmakov <shmakovvadim@mail.ru>
2021-01-30 19:30:07 +0500

    Revert "[task] Reverted extra condition"
    This reverts commit 2cd8c0529336c1acd6638282efb9cd545ca81773.

--- src/Specification/Task/IsViewable.php
+++ src/Specification/Task/IsViewable.php
@@ -7,10 +7,13 @@ namespace App\Specification\Task;
 use App\Entity\User;
 use App\Security\CurrentUserProvider;
 use App\Specification\AlwaysSpecified;
+use App\Specification\AndX;
 use App\Specification\CompositeSpecification;
 use App\Specification\Equals;
 use App\Specification\Join;
 use App\Specification\MemberOf;
+use App\Specification\Not;
+use App\Specification\Project\IsArchived;
 use App\Specification\Specification;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
@@ -26,14 +29,23 @@ final class IsViewable extends CompositeSpecification
             return new AlwaysSpecified();
         }
 
+        $isNotArchived = new Not(new IsArchived());
         $user = $this->currentUserProvider->getUser();
 
         if ($this->authorizationChecker->isGranted(User::ROLE_MANAGER)) {
             $isProjectMember = new MemberOf('members', $user);
 
-            return new Join('task', 'project', $isProjectMember);
+            return $this->getProjectSpecification(new AndX($isNotArchived, $isProjectMember));
         }
 
-        return new Equals('performedBy', $user);
+        return new AndX(
+            new Equals('performedBy', $user),
+            $this->getProjectSpecification($isNotArchived)
+        );
+    }
+
+    private function getProjectSpecification(Specification $specification): Join
+    {
+        return new Join('task', 'project', $specification);
     }
 }
